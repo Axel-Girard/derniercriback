a68cd26ebd97dd54898ca2aa92e7eb7a32015a3c
Axel <axel942@gmail.com>
2021-04-05 05:08:16 +0200

    add basic credential test

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    const login = req.body.login;
+    const password = req.body.password;
+
+    if ( login === 'dernier' && password === 'cri') {
+        res.send('Log in Dernier Cri!')
+    } else {
+        res.status(401).send('Login or password incorrect')
+    }
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
